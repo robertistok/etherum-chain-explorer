af4402cd86a57e68011e6ef0d8d7d3327fd3305f
robertistok <robertistok94@gmail.com>
2019-10-20 14:18:34 +0800

    Add pagination

--- src/components/LatestBlocks/LatestBlocks.js
+++ src/components/LatestBlocks/LatestBlocks.js
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useTransition } from "react-spring";
+import nanoid from "nanoid";
 
 import { Table, TableHeader, TableRow } from "@aragon/ui";
 
 import { Title } from "../common";
 import BlockRow from "./BlockRow";
+import Pagination from "./Pagination";
 
 import { useLatestBlocksStateValue } from "../../state/latestBlocks";
 import { formatTime } from "../../utils/time";
 
+const BLOCKS_PER_PAGE = 10;
+
 const LatestBlocks = () => {
   const { latestBlocks, lastUpdated } = useLatestBlocksStateValue();
+  const [page, setPage] = useState(1);
 
-  const transitions = useTransition(latestBlocks, block => block.hash, {
-    from: item => ({
-      opacity: 0,
-      transform: `translate3d(${item.number % 2 ? "-100%" : "100%"},0,0)`
-    }),
-    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
-    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
-  });
+  const transitions = useTransition(
+    latestBlocks,
+    block => (block ? block.hash : nanoid()),
+    {
+      from: item => ({
+        opacity: 0,
+        transform: `translate3d(${item.number % 2 ? "-100%" : "100%"},0,0)`
+      }),
+      enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+      leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
+    }
+  );
 
   return (
     <Root>
-      <Title>Latest blocks</Title>
-      <span>
-        <em>{`Last updated: ${
-          lastUpdated ? formatTime({ timestamp: lastUpdated }) : "never"
-        }`}</em>
-      </span>
+      <Title>
+        Latest blocks (updated:&nbsp;
+        <em>
+          {`${lastUpdated ? formatTime({ timestamp: lastUpdated }) : "never"}`}
+        </em>
+        )
+      </Title>
+
+      <Pagination
+        items={latestBlocks}
+        itemsPerPage={BLOCKS_PER_PAGE}
+        page={page}
+        setPage={setPage}
+      />
+
       <StyledTable
         header={
           <TableRow>
@@ -41,9 +59,11 @@ const LatestBlocks = () => {
           </TableRow>
         }
       >
-        {transitions.map(({ item, props: animationProps, key }) => (
-          <BlockRow key={key} animationProps={animationProps} {...item} />
-        ))}
+        {transitions
+          .slice((page - 1) * BLOCKS_PER_PAGE, BLOCKS_PER_PAGE * page)
+          .map(({ item, props: animationProps, key }) => (
+            <BlockRow key={key} animationProps={animationProps} {...item} />
+          ))}
       </StyledTable>
     </Root>
   );
---
+++ src/components/LatestBlocks/Pagination/Pagination.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Button, Text } from "@aragon/ui";
+
+const Pagination = ({ items = [], itemsPerPage = 10, page = 1, setPage }) => {
+  const totalItems = items.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage) || 1;
+
+  const handlePageChange = event => {
+    const { name } = event.target;
+
+    if (name === "prev") {
+      setPage(page - 1);
+    } else if (name === "next") {
+      setPage(page + 1);
+    }
+  };
+
+  return (
+    <Root>
+      <Button
+        aria-label="Previous page"
+        disabled={page === 1}
+        name="prev"
+        mode="strong"
+        onClick={handlePageChange}
+        tabindex="0"
+      >
+        Previous
+      </Button>
+      <Text aria-label={`Page ${page} of ${totalPages}`} smallcaps>
+        Page {page} of {totalPages}
+      </Text>
+      <Button
+        aria-label="Next page"
+        disabled={totalItems <= itemsPerPage * page}
+        name="next"
+        mode="strong"
+        onClick={handlePageChange}
+        tabindex="0"
+      >
+        Next
+      </Button>
+    </Root>
+  );
+};
+
+Pagination.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+  itemsPerPage: PropTypes.number,
+  page: PropTypes.number,
+  setPage: PropTypes.func
+};
+
+Pagination.defaultProps = {
+  items: [],
+  itemsPerPage: 10,
+  page: 1
+};
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px 0px 0px;
+`;
+
+export default Pagination;
