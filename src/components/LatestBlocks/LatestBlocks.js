de9ae80204d13ac6b290019df367490bb8ed1b56
robertistok <robertistok94@gmail.com>
2019-10-21 11:20:59 +0800

    Simplify animation

--- src/components/LatestBlocks/LatestBlocks.js
+++ src/components/LatestBlocks/LatestBlocks.js
@@ -24,7 +24,7 @@ const LatestBlocks = () => {
     {
       from: item => ({
         opacity: 0,
-        transform: `translate3d(${item.number % 2 ? "-100%" : "100%"},0,0)`
+        transform: `translate3d(-100%,0,0)`
       }),
       enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
       leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
