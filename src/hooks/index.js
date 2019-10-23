3933bc66bd16af4ffc32f58a1a8c34b84b97f632
robertistok <robertistok94@gmail.com>
2019-10-23 14:43:02 +0800

    Implement useDataApi hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useInterval } from "./useInterval";
 export { default as usePrevious } from "./usePrevious";
+export { default as useDataApi } from "./useDataApi";
---
+++ src/hooks/useDataApi.js
@@ -0,0 +1,28 @@
+// inspiration https://www.robinwieruch.de/react-hooks-fetch-data
+import { useState, useEffect, useCallback } from "react";
+
+const useDataApi = ({ dataFetcher, initialData }) => {
+  const [data, setData] = useState(initialData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setIsError(false);
+    setIsLoading(true);
+    try {
+      const result = await dataFetcher();
+      setData(result);
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  }, [dataFetcher]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, dataFetcher]);
+
+  return [{ data, isLoading, isError }, fetchData];
+};
+
+export default useDataApi;
