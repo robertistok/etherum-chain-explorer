b8e03e265d8247685094f3ed109bb1ab0f5aa7dc
robertistok <robertistok94@gmail.com>
2019-10-24 18:51:41 +0800

    Add utilities,
    getEtherValueOfBlock
    ETHER_VALUE_IN_USD
    getEtherValueInUSD

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,5 +1,8 @@
 import { default as _range } from "lodash-es/range";
 
+// value, this should be kept up to date ideally
+const ETHER_VALUE_IN_USD = 160.1;
+
 // partial source
 // https://ethereum.stackexchange.com/questions/1587/how-can-i-get-the-data-of-the-latest-10-blocks-via-web3-js
 export const getLatestNBlocks = async ({
@@ -29,5 +32,15 @@ export const getLatestNBlocks = async ({
   });
 };
 
-export const formatTransactionHash = hash =>
+export const formatHash = hash =>
   `${hash.slice(0, 4)}...${hash.slice(hash.length - 4)}`;
+
+export const getEtherValueOfBlock = block =>
+  block && block.transactions
+    ? block.transactions
+        .reduce((acc, t) => acc + Number(window.web3.utils.fromWei(t.value)), 0)
+        .toFixed(2)
+    : null;
+
+export const getEtherValueInUSD = value =>
+  (value * ETHER_VALUE_IN_USD).toFixed(2);
